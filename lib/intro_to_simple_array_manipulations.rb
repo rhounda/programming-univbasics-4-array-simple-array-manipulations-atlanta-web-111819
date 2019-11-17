29523797c671c6bd9bf78ee7578a740bfa1dadb7
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 04:29:21 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,33 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end 
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_concat(array, array2)
+  array.concat(array2)
+  
+def using_insert(array, element)
+  
+def using_uniq(array)
+  array.flatten
+  
+def using_delete(array, insert)
+  array.delete(insert)
+  
+def using_delete_at(array, integer)
+  array.delete(interger)
+  
+  
+  
+  
