97835560d596a06e351ce40c20c1c15b0040b836
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 04:29:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,7 +14,20 @@ def pop_with_args(array)
   array.pop(2)
 end
 
-def using_concat(array array2)
+def using_concat(array, array2)
   array.concat(array2)
+  
+def using_insert(array, element)
+  
+def using_uniq(array)
+  array.flatten
+  
+def using_delete(array, insert)
+  array.delete(insert)
+  
+def using_delete_at(array, integer)
+  array.delete(interger)
+  
+  
   
   
